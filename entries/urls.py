a6904fbdf69b1c802e349aff33a9054aef304b8f
Peter Brown <peterbrown454@gmail.com>
2024-03-11 00:16:14 +0000

    sluf slug set up

--- entries/urls.py
+++ entries/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('', views.entry_list),
+    path('<slug:slug>/', views.entry_detail, name='entry_detail'),
     
 ]
--- entries/views.py
+++ entries/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Entry
+from django.http import HttpResponse
 
 # Create your views here.
 
@@ -7,3 +8,7 @@ from .models import Entry
 def entry_list(request):
     entries = Entry.objects.all().order_by('created_on')
     return render(request, 'entry_list_template.html', {'entries': entries})
+
+def entry_detail(request, slug):
+    return HttpResponse(slug)
+
