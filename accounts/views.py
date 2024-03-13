86d3280c7494824b7ba66dfb0bbebfcb8c150be1
Peter Brown <peterbrown454@gmail.com>
2024-03-13 01:31:58 +0000

    sign up template rendering!

--- accounts/views.py
+++ accounts/views.py
@@ -4,4 +4,4 @@ from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 def signup_view(request):
     form = UserCreationForm()
-    return render(request, "signup.html",) 
+    return render(request, "signup.html", {'form':form}) 
