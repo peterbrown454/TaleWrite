690928474a611efe60d1bd1c1a4a6e0ac96074d5
Peter Brown <peterbrown454@gmail.com>
2024-03-18 07:55:45 +0000

    like button edited

--- entries/forms.py
+++ entries/forms.py
@@ -5,3 +5,7 @@ class WriteEntry (forms.ModelForm):
     class Meta:
         model = models.Entry
         fields = ['title', 'content', 'excerpt','slug', 'thumb']
+
+    widgets = {
+        'content': forms.Textarea(attrs={'rows': 40, 'cols': 2}),  # Adjust rows and cols as needed
+    }
