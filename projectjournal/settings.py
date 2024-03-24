7736493f49e65ef08794c54eb56c038dee216ceb
Peter Brown <peterbrown454@gmail.com>
2024-03-24 18:52:57 +0000

    collected static

--- projectjournal/settings.py
+++ projectjournal/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY =  os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['journap-c86fda53e915.herokuapp.com', '8000-peterbrown454-diary-b6qovr99y0h.ws-eu110.gitpod.io', 'gitpod.io', 'herokuapp.com',]
 
 
