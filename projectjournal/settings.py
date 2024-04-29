19fb85ecd4c671ed13ba1a1230c8b196b51a55de
Peter Brown <peterbrown454@gmail.com>
2024-04-29 13:13:40 +0000

    allowed hosts edited

--- projectjournal/settings.py
+++ projectjournal/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY =  os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['journap-c86fda53e915.herokuapp.com', '8000-peterbrown454-diary-b6qovr99y0h.ws-eu110.gitpod.io', 'gitpod.io', 'herokuapp.com', '8000-peterbrown454-diary-ewoyturoyqk.ws-eu110.gitpod.io']
+ALLOWED_HOSTS = ['journap-15c9108ba52d.herokuapp.com', 'journap-c86fda53e915.herokuapp.com', '8000-peterbrown454-diary-b6qovr99y0h.ws-eu110.gitpod.io', 'gitpod.io', 'herokuapp.com', '8000-peterbrown454-diary-ewoyturoyqk.ws-eu110.gitpod.io']
 
 
 # Application definition
